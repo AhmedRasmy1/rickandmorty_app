931f25716f60eaefe439392a6a09792401b450c6
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-20 10:21:32 +0300

    create Characters derails Screen and put baseUrl and sone constant things

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:rickandmorty_app/constants/strings.dart';
+import 'package:rickandmorty_app/presentation/screens/characters_details_screen.dart';
 import 'package:rickandmorty_app/presentation/screens/characters_screen.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case charactersScreen:
         return MaterialPageRoute(builder: (_) => const CharactersScreen());
+
+      case charactersDetailsScreen:
+        return MaterialPageRoute(
+            builder: (_) => const CharactersDetailsScreen());
     }
+
     return null;
   }
 }
